45223f6d34054276406ee3d7310ab9379825901f
Jessica Barclay <jessicabarclay.net@gmail.com>
2025-06-14 16:02:30 +0100

    Add read replicas in memory

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Leader based replication - in Golang
 
 ![basic-architecture](basic-architecture.png)
+
+Run the application:
+```go
+go run main.go
+```
+
+Write example:
+
+```http
+curl -X POST http://localhost:8080/write \
+  -H "Content-Type: application/json" \
+  -d '{"key": "user:1", "value": "Jessica"}'
+```
+
+Read from the Leader:
+```http
+curl "http://localhost:8080/read?key=user:1"
+```
+
+Read from specific follower(simulates some delay). If you try this right after writing you may get a 404 if replication hasn’t completed yet:
+
+```http
+curl "http://localhost:8080/follower-read?key=user:1&replica=1"
+```
--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"go-replication-simulation/store"
 )
 
-var leaderStore = store.NewStore()
+var (
+	leaderStore    = store.NewStore()
+	followerStores = []*store.Store{
+		store.NewStore(), // follower 1
+		store.NewStore(), // follower 2
+	}
+)
 
 type WriteRequest struct {
 	Key   string `json:"key"`
@@ -33,9 +40,13 @@ func writeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func replicateToFollowers(req WriteRequest) {
-	// In real life you'd do HTTP POST to follower nodes here
-	time.Sleep(500 * time.Millisecond) // Simulate lag
-	log.Printf("[ASYNC] Replicating key=%s to followers", req.Key)
+	for i, follower := range followerStores {
+		go func(idx int, f *store.Store) {
+			time.Sleep(time.Duration(200+idx*300) * time.Millisecond) // staggered delay
+			f.Set(req.Key, req.Value)
+			log.Printf("[ASYNC] Replicated key=%s to follower %d", req.Key, idx+1)
+		}(i, follower)
+	}
 }
 
 func readHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,10 +59,31 @@ func readHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(rec)
 }
 
+func followerReadHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	replicaStr := r.URL.Query().Get("replica")
+
+	replicaNum, err := strconv.Atoi(replicaStr)
+	if err != nil || replicaNum < 1 || replicaNum > len(followerStores) {
+		http.Error(w, "invalid replica number", http.StatusBadRequest)
+		return
+	}
+
+	idx := replicaNum - 1
+	rec, ok := followerStores[idx].Get(key)
+	if !ok {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(rec)
+}
+
 func main() {
 	http.HandleFunc("/write", writeHandler)
 	http.HandleFunc("/read", readHandler)
+	http.HandleFunc("/follower-read", followerReadHandler)
 
-	log.Println("Leader running at http://localhost:8080")
+	log.Println("Application running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
